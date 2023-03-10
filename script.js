dc5c70e0b1971b47ff48656b16eeb0cb3d57fef0
saurabh-shete <ss.saurabhshete@gmail.com>
2023-03-11 01:05:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function toggleDropdown () {
+//   var dropdown = document.getElementById('myDropdown')
+//   dropdown.classList.toggle('show')
+// }
+// window.onclick = function (event) {
+//   // Get the dropdown element
+//   var dropdown = document.getElementById('myDropdown')
+//   //If the clicked element is not part of the dropdown, hide the dropdown
+//   if (
+//     !event.target.matches('a') ||
+//     (event.target.matches('a') != true &&
+//       dropdown.contains(event.target) != true)
+//   ) {
+//     dropdown.classList.remove('show')
+//   }
+// }
